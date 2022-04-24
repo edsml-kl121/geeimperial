f1bf922773bf16d17cd5433cc8dc186ed759af37
edsml-kl121 <yugiohtra@gmail.com>
2022-04-24 14:46:00 +0100

    fix production

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ app.use(cors())
 app.use(express.json())
 
 // static file declaration
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
   // production mode
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+  // }
+  app.use("/api/v1/apps", apps)
+  app.use(express.static("client/build"));
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.path(__dirname, '../build'));
+  // });
+  app.use("*", (req, res) => res.status(404).json({ error : "not found"}))
 
-app.use("/api/v1/apps", apps)
-app.use("*", (req, res) => res.status(404).json({ error : "not found"}))
 
 export default app
